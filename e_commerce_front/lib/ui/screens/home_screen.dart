2d847d360e1dfb867d23a6167399d7dc0978d781
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-05-30 17:50:55 +0330

    additional documents

--- e_commerce_front/lib/ui/screens/home_screen.dart
+++ e_commerce_front/lib/ui/screens/home_screen.dart
@@ -10,6 +10,52 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            color: Colors.blue,
+            height: 60.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Menu 1',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Menu 2',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Menu 3',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: List.generate(50, (index) {
+                  return ListTile(
+                    title: Text('Item ${index + 1}'),
+                  );
+                }),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
