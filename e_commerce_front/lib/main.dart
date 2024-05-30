51115985b6aa69066f6743c4b7847c96565c0abb
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-05-30 16:13:54 +0330

    initial commit

---
+++ e_commerce_front/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:e_commerce_front/ui/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Candleaf',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+
