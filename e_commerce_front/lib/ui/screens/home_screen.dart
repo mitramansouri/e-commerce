a19ed0a75133a23e853a4f18d17cee2c364a9c96
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-05-31 16:59:16 +0330

    Home page Heade

--- e_commerce_front/lib/ui/screens/home_screen.dart
+++ e_commerce_front/lib/ui/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'package:dropdown_button2/dropdown_button2.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,37 +12,156 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final List<String> items = [
+    'Item1',
+    'Item2',
+    'Item3',
+    'Item4',
+    'Item5',
+    'Item6',
+    'Item7',
+    'Item8',
+  ];
+  String? selectedValue;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: <Widget>[
+          // Fixed Menu while scrolling down
           Container(
-            color: Colors.blue,
+            color: Colors.white,
             height: 60.0,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: <Widget>[
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Menu 1',
-                    style: TextStyle(color: Colors.white),
+                //icons
+                InkWell(
+                  child: Row(
+                    children: [
+                      //candleaf icon
+                      SvgPicture.asset(
+                        'assets/icons/candleaf-icon.svg',
+                        width: 40.0, // Set the desired width
+                        height: 40.0, // Set the desired height
+                      ),
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      // candleaf name icon
+                      SvgPicture.asset(
+                        'assets/icons/wordmark.svg',
+                        width: 20.0, // Set the desired width
+                        height: 20.0, // Set the desired height
+                      ),
+                    ],
                   ),
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Menu 2',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                // menu items
+                Row(
+                  children: [
+                    // discover drop down
+                    DropdownButtonHideUnderline(
+                      child: DropdownButton2<String>(
+                        isExpanded: true,
+                        hint: const Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Expanded(
+                              child: Text(
+                                'Discovery',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        items: items
+                            .map((String item) => DropdownMenuItem<String>(
+                                  value: item,
+                                  child: Text(
+                                    item,
+                                    style: const TextStyle(
+                                      color: Colors.black,
+                                    ),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ))
+                            .toList(),
+                        value: selectedValue,
+                        onChanged: (String? value) {
+                          setState(() {
+                            selectedValue = value;
+                          });
+                        },
+                        buttonStyleData: const ButtonStyleData(
+                          width: 120,
+                          padding: EdgeInsets.only(left: 14, right: 14),
+                        ),
+                        iconStyleData: const IconStyleData(
+                          icon: Icon(
+                            Icons.keyboard_arrow_down_sharp,
+                          ),
+                          iconSize: 20,
+                          iconEnabledColor: Colors.black,
+                          iconDisabledColor: Colors.grey,
+                        ),
+                        dropdownStyleData: DropdownStyleData(
+                          maxHeight: 200,
+                          width: 200,
+                          offset: const Offset(-20, 0),
+                          scrollbarTheme: ScrollbarThemeData(
+                            radius: const Radius.circular(40),
+                            thickness: MaterialStateProperty.all<double>(6),
+                            thumbVisibility:
+                                MaterialStateProperty.all<bool>(true),
+                          ),
+                        ),
+                        menuItemStyleData: const MenuItemStyleData(
+                          height: 40,
+                          padding: EdgeInsets.only(left: 14, right: 14),
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'About',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Contact Us',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
                 ),
-                TextButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Menu 3',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Profile Icon
+                    IconButton(
+                      icon: const Icon(Icons.person_2_outlined),
+                      tooltip: 'See Profile',
+                      onPressed: () {
+                        setState(() {});
+                      },
+                    ),
+                    //shopping cart Icon
+                    IconButton(
+                      icon: const Icon(Icons.shopping_cart_outlined),
+                      tooltip: 'See cart',
+                      onPressed: () {
+                        setState(() {});
+                      },
+                    ),
+                  ],
                 ),
               ],
             ),
@@ -46,11 +169,9 @@ class _HomeScreenState extends State<HomeScreen> {
           Expanded(
             child: SingleChildScrollView(
               child: Column(
-                children: List.generate(50, (index) {
-                  return ListTile(
-                    title: Text('Item ${index + 1}'),
-                  );
-                }),
+                children: [
+                  
+                ],
               ),
             ),
           ),
