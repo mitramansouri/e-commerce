9d1665977159cb9a293685c4024b1c1c69071716
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-06-15 17:54:19 +0330

    offers done

--- e-commerce-front-react/src/Pages/Shop.jsx
+++ e-commerce-front-react/src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from '../Components/Hero/Hero';
 import Popular from "../Components/Popular/Popular";
+import Offer from "../Components/Offers/Offer";
 //home page
 const Shop = () =>{
     return(
         <div>
             <Hero/>
             <Popular/>
+            <Offer/>
         </div>
     )
 }
