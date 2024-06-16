4553887f541b4499e49a78f0ae9e31d080e4a3e2
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-06-16 17:09:25 +0330

    signup page is ready

--- e-commerce-front-react/src/Pages/LoginSignUp.jsx
+++ e-commerce-front-react/src/Pages/LoginSignUp.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import './CSS/LoginSignup.css'
 
 const LoginSignUp = () =>{
     return(
-        <div>
-
+        <div className="loginSignup">
+            <div className="loginSignup-container">
+                <h1>Sign Up</h1>
+                <div className="loginSignup-fields">
+                    <input type="text" placeholder="Your Name" />
+                    <input type="emial"  placeholder="Email adress"/>
+                    <input type="password" placeholder="Password" />
+                </div>
+                <button>
+                    Continue
+                </button>
+                <p className="loginSignup-login">
+                    Already have an account? <span>Login here</span>
+                </p>
+                <div className="loginSignup-agree">
+                    <input type="checkbox" name="" id="" />
+                    <p>By continuing, i agree to choose the terms of use and priavacy and policy.</p>
+                </div>
+            </div>
         </div>
     )
 }
