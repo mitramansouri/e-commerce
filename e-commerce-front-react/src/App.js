e560371a0b56d2d50162beb6b5d11067d67058e1
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-06-16 12:14:15 +0330

    footer

--- e-commerce-front-react/src/App.js
+++ e-commerce-front-react/src/App.js
@@ -6,6 +6,7 @@ import Product from './Pages/Product';
 import Shop from './Pages/Shop';
 import Cart from './Pages/Cart';
 import LoginSignUp from './Pages/LoginSignUp';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div>
@@ -24,6 +25,7 @@ function App() {
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/login' element={<LoginSignUp/>}/>
       </Routes>
+      <Footer/>
       </BrowserRouter>
     </div>
   );
