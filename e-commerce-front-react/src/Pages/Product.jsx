2c91074058e76d1e909608d935d41463082bd823
Mitra Mansouri <56555397+mitramansouri@users.noreply.github.com>
2024-06-17 15:10:28 +0330

    breadcrums

--- e-commerce-front-react/src/Pages/Product.jsx
+++ e-commerce-front-react/src/Pages/Product.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from '../Context/ShopContext.jsx';
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum.jsx";
 
 const Product = () =>{
+    const {all_product} = useContext(ShopContext);
+    const {productID} = useParams();
+    const product =all_product.find((e)=>e.id === Number(productID));
     return(
         <div>
-
+            <Breadcrum product={product}/>
         </div>
     )
 }
